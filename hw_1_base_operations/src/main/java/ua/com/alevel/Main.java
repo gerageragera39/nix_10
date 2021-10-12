0ecd14ae2aa1ab908cfa6b7a60c264e1520f932a
DESKTOP-IEH9GGS\admin <german030103@gmail.com>
2021-10-13 02:34:13 +0300

    interface

--- hw_1_base_operations/src/main/java/ua/com/alevel/Main.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/Main.java
@@ -1,69 +1,69 @@
 package ua.com.alevel;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String position = "1";
+        String taskNum;
+        while (position != null) {
+            switch (position) {
+                case "0": {
+                    System.exit(0);
+                }
+                case "1": {
+                    System.out.print("Enter the number of task : ");
+                    taskNum = scanner.nextLine();
+                    logic(taskNum);
+                }
+            }
+            System.out.println("If you want to continue working, enter '1', if not, then '0'");
+            position = scanner.nextLine();
+        }
+
+    }
+
+    private static void logic(String taskNum) {
+        switch (taskNum) {
+            case "1":
+                task1();
+                break;
+            case "2":
+                task2();
+                break;
+            case "3":
+                task3();
+                break;
+        }
+    }
+
+    private static void task1() {
         Task1 t1 = new Task1();
         t1.srchSum();
-        System.out.println("\n");
-        Task2 t2 = new Task2();
-        t2.quantSrch();
-        System.out.println("\n");
-        Task3 t3 = new Task3();
-        t3.endTime();
+    }
+
+    private static void task2() {
+        Task2 task2 = new Task2();
+        task2.quantSrch();
+    }
+
+    private static void task3() {
+        Task3 task3 = new Task3();
+        task3.endTime();
     }
 }
 
-
-
-//package ua.com.alevel;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//
-//public class Main {
-//
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//        System.out.println("Select you event");
-//        String position;
-//        while ((position = reader.readLine()) != null) {
-//            logic(position, reader);
-//            position = reader.readLine();
-//            switch (position) {
-//                case "0" : {
-//                    System.exit(0);
-//                }
-//                case "1" : {
-//                    logic(position, reader);
-//                }
-//            }
-//        }
-//        reader.close();
-//    }
-//
-//    private static void logic(String position, BufferedReader bufferedReader) {
-//        switch (position) {
-//            case "1" : task1(); break;
-//            case "2" : task2(); break;
-//            case "3" : task3(); break;
-//        }
-//        System.out.println("Your variant: if you want exit, please input 0, else, repeat logic");
-//    }
-//
-//    private static void task1() {
 //        Task1 t1 = new Task1();
-//        t1.task();
-//    }
-//
-//    private static void task2() {
-//        Task2 task2 = new Task2();
-//        task2.task();
-//    }
-//
-//    private static void task3() {
-//        Task3 task3 = new Task3();
-//        task3.task();
-//    }
-//}
+//        t1.srchSum();
+//        System.out.println("\n");
+//        Task2 t2 = new Task2();
+//        t2.quantSrch();
+//        System.out.println("\n");
+//        Task3 t3 = new Task3();
+//        t3.endTime();
+
+
+
+
+
--- hw_1_base_operations/src/main/java/ua/com/alevel/Task1.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/Task1.java
@@ -7,7 +7,7 @@ public class Task1 {
     public void srchSum(){
         int sum = 0, dig = 0;
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the string ");
+        System.out.print("Enter the string : ");
         String str = in.nextLine();
         for (int i = 0; i < str.length(); i++) {
             if (Character.isDigit(str.charAt(i))) {
