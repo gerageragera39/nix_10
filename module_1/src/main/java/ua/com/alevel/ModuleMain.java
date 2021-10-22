9dc63cd4740c4ee1b70e06c7bf6766199a943ba4
DESKTOP-IEH9GGS\admin <german030103@gmail.com>
2021-10-22 22:46:42 +0300

    module_1

---
+++ module_1/src/main/java/ua/com/alevel/ModuleMain.java
@@ -0,0 +1,10 @@
+package ua.com.alevel;
+
+import ua.com.alevel.level_1.Level1Main;
+
+public class ModuleMain {
+    public static void main(String[] args) {
+        Level1Main level1 = new Level1Main();
+        level1.runlevel1();
+    }
+}
---
+++ module_1/src/main/java/ua/com/alevel/level_1/Level1Main.java
@@ -0,0 +1,10 @@
+package ua.com.alevel.level_1;
+
+public class Level1Main {
+
+    private final NumberOfUniqueCharacters numberOfUniqueCharacters = new NumberOfUniqueCharacters();
+
+    public void runlevel1() {
+        System.out.println(numberOfUniqueCharacters.findNumOfUniqueChar());
+    }
+}
